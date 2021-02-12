cae3e10b54b59c314dc68756f72ee26460d20310
anaistatibouet <darkana.polofowl@gmail.com>
2021-02-12 02:17:03 +0100

    fix sum export function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const sum = require("./functions/sum");
 
-let res = sum(1,2).toString()
-console.log(res)
+require('dotenv').config()
 
+const express = require('express')
+const app = express();
+const PORT = 8080;
+var tool = require('./functions/sum');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+  
+app.get('/sum', (req, res) => {
+    let sumResult = tool.sum(1, 18);
+    res.send(
+        `The sum is: ${sumResult.toString()}`
+    )
+})
+  
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
