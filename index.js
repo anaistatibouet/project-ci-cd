cee0cdf4cfc729cd17fa22bf999eee68c6912a45
anaistatibouet <darkana.polofowl@gmail.com>
2021-02-12 02:24:28 +0100

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 require('dotenv').config()
 
-const express = require('express')
-const app = express();
+const express = require('express');
+const tool = require('./functions/sum');
 const PORT = 8080;
-var tool = require('./functions/sum');
+const app = express();
+
+app.disable("x-powered-by");
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
